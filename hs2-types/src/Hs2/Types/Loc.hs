7bb7a759022a05f848886d20f342ac60d15e888f
Sandy Maguire <sandy@sandymaguire.me>
2020-04-18 02:41:06 -0700

    Force same K1 parameters in GBimap

--- hs2-types/src/Hs2/Types/Loc.hs
+++ hs2-types/src/Hs2/Types/Loc.hs
@@ -39,25 +39,25 @@ instance (GBimap a b c d fi fo, GBimap a b c d gi go) => GBimap a b c d (fi :+: 
   gbimap f g (L1 a) = L1 $ gbimap f g a
   gbimap f g (R1 b) = R1 $ gbimap f g b
 
-instance {-# INCOHERENT #-} GBimap a b c d (K1 _1 a) (K1 _2 b) where
+instance {-# INCOHERENT #-} GBimap a b c d (K1 _1 a) (K1 _1 b) where
   gbimap f _ (K1 a) = K1 $ f a
 
-instance {-# INCOHERENT #-} GBimap a b c d (K1 _1 c) (K1 _2 d) where
+instance {-# INCOHERENT #-} GBimap a b c d (K1 _1 c) (K1 _1 d) where
   gbimap _ g (K1 b) = K1 $ g b
 
-instance {-# INCOHERENT #-} Bifunctor p => GBimap a b c d (K1 _1 (Loc c (p a c))) (K1 _2 (Loc d (p b d))) where
+instance {-# INCOHERENT #-} Bifunctor p => GBimap a b c d (K1 _1 (Loc c (p a c))) (K1 _1 (Loc d (p b d))) where
   gbimap f g (K1 loc) = K1 $ bimap g (bimap f g) loc
 
-instance {-# INCOHERENT #-} Bifunctor p => GBimap a b c d (K1 _1 (p a c)) (K1 _2 (p b d)) where
+instance {-# INCOHERENT #-} Bifunctor p => GBimap a b c d (K1 _1 (p a c)) (K1 _1 (p b d)) where
   gbimap f g (K1 loc) = K1 $ bimap f g loc
 
-instance {-# INCOHERENT #-} GBimap a b c d (K1 _1 x) (K1 _2 x) where
+instance {-# INCOHERENT #-} GBimap a b c d (K1 _1 x) (K1 _1 x) where
   gbimap _ _ (K1 x) = K1 x
 
-instance (Functor q, GBimap a b c d (K1 _1 i) (K1 _2 o))
-    => GBimap a b c d (K1 _1 (q i)) (K1 _2 (q o)) where
+instance (Functor q, GBimap a b c d (K1 _1 i) (K1 _1 o))
+    => GBimap a b c d (K1 _1 (q i)) (K1 _1 (q o)) where
   gbimap f g (K1 loc) =
-    K1 $ fmap (unK1 . gbimap @_ @_ @_ @_ @(K1 _1 i) @(K1 _2 o) f g . K1) loc
+    K1 $ fmap (unK1 . gbimap @_ @_ @_ @_ @(K1 _1 i) @(K1 _1 o) f g . K1) loc
 
 glocmap
   :: (GBimap a1 b c1 d (Rep a2) (Rep c2), Generic c2, Generic a2) =>
